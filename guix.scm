3d8174f4242d30d08e190f06c7cdfc18d9b0ea26
Jonathan D.A. Jewell <6759885+hyperpolymath@users.noreply.github.com>
2026-07-26 16:04:56 +0100

    chore: update guix.scm from squisher-corpus

--- guix.scm
+++ guix.scm
@@ -1,25 +1,18 @@
-;; social-media-polygraph - Guix Package Definition
-;; Run: guix shell -D -f guix.scm
+; SPDX-License-Identifier: MPL-2.0
+;; guix.scm — GNU Guix package definition for squisher-corpus
+;; Usage: guix shell -f guix.scm
 
 (use-modules (guix packages)
-             (guix gexp)
-             (guix git-download)
              (guix build-system gnu)
-             ((guix licenses) #:prefix license:)
-             (gnu packages base))
+             (guix licenses))
 
-(define-public social_media_polygraph
-  (package
-    (name "social-media-polygraph")
-    (version "0.1.0")
-    (source (local-file "." "social-media-polygraph-checkout"
-                        #:recursive? #t
-                        #:select? (git-predicate ".")))
-    (build-system gnu-build-system)
-    (synopsis "Guix channel/infrastructure")
-    (description "Guix channel/infrastructure - part of the RSR ecosystem.")
-    (home-page "https://github.com/hyperpolymath/social-media-polygraph")
-    (license license:agpl3+)))
-
-;; Return package for guix shell
-social_media_polygraph
+(package
+  (name "squisher-corpus")
+  (version "0.1.0")
+  (source #f)
+  (build-system gnu-build-system)
+  (synopsis "squisher-corpus")
+  (description "squisher-corpus — part of the hyperpolymath ecosystem.")
+  (home-page "https://github.com/hyperpolymath/squisher-corpus")
+  (license ((@@ (guix licenses) license) "PMPL-1.0-or-later"
+             "https://github.com/hyperpolymath/palimpsest-license")))
